726e7151173a0000c29677263c369860cf0fc9ed
mikewqq <mike.w.github@mail.ru>
2018-06-27 01:35:13 +0500

    add htmlwebpackplugin and loading assets config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("loaded");
+// import * as React from "react";
+// import {render} from "react-dom";
+import "./index.css";
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+root.className = "hello";
+root.innerText = "Hello world";
+
+
+// const App = () => <h1>Hello world</h1>;
+//
+// render(<App/>, root);
